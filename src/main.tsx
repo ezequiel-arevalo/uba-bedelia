5e435bae376266990d00624f77a82efe1b65a21d
Ezequiel Arevalo <ezequiel-arevalo@outlook.com>
2025-08-22 13:17:21 -0300

    fix: adjusting for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,6 @@ import { Toaster } from 'sonner';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <Toaster position="top-right" />
+    <Toaster position="bottom-center" />
   </StrictMode>
 );
